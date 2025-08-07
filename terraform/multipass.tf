54de20f9ccefeab40d7dfb36e5504dd8b8ac6e79
keroles-nadyy <keroles_N@outlook.com>
2025-08-07 23:56:17 +0300

    update terraform code

--- terraform/multipass.tf
+++ terraform/multipass.tf
@@ -4,18 +4,11 @@ resource "multipass_instance" "master_node" {
   memory = "1G"
   disk   = "4G"
 #   image  = "20.04"  #latest cloud_init = <<-EOF
-
-  # cloudinit_file = "./instances_cloud-init.yaml"
 }
-
-# output "master_node_ip" {
-#   value = multipass_instance.master_node.ipv4
-# }
 
 resource "multipass_instance" "worker_node" {
   name = "Worker"
   cpus   = 2
   memory = "512M"
   disk   = "4G"
-  # cloudinit_file = "./instances_cloud-init.yaml"
 }
