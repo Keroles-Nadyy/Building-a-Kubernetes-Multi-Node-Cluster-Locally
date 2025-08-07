14dee72a69c719d8c22390d29b71530dd6fb2cff
keroles-nadyy <keroles_N@outlook.com>
2025-08-07 23:53:18 +0300

    update script

--- scripts/multipass_instances_Ips.sh
+++ scripts/multipass_instances_Ips.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Define the inventory file
+# Define the inventory file.
 inventory_file="./../ansible/inventory"
-
 
 # Empty the old inventory file
 > $inventory_file
@@ -11,11 +10,15 @@ inventory_file="./../ansible/inventory"
 echo -e "[my_machine]\nlocalhost" >> $inventory_file
 
 # Add master section
-echo -e "\n[master]" >> $inventory_file
+echo -e "\n[master_nodes]" >> $inventory_file
 multipass list | awk 'NR>1 && $1 ~ /Master/ && $2=="Running" {print $3}' >> $inventory_file
 
 # Add worker section
-echo -e "\n[worker]" >> $inventory_file
+echo -e "\n[worker_nodes]" >> $inventory_file
 multipass list | awk 'NR>1 && $1 ~ /Worker/ && $2=="Running" {print $3}' >> $inventory_file
+
+# Add ALL nodes section
+echo -e "\n[K8s_cluster_nodes:children]\nmaster_nodes\nworker_nodes" >> $inventory_file
+# multipass list | awk 'NR>1 && ($1 ~ /Worker/ || $1 ~ /Master/) && $2=="Running" {print $3}' >> $inventory_file
 
 echo "Inventory file generated: $inventory_file"
